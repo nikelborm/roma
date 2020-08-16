80c78e86dd9813508b3903996d0f955d487920b4
nikelborm <kolya007.klass@gmail.com>
2020-08-17 01:03:21 +0300

    Adding imitation of DB

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  state = {
+    isUsersDownloaded : false,
+    users : []
+  }
+  componentDidMount = async () => {
+    const response = await fetch(document.location.origin + "/users.json", {
+      method: "get",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+    const users = await response.json();
+    console.log('users: ', users);
 
-function App() {
-  return (
-    <div >
-    </div>
-  );
+    this.setState( {
+      isUsersDownloaded : true,
+      users
+    } );
+  }
+  render() {
+    console.log('this.dataCache: ', this.dataCache);
+    return (
+      <div>
+        { this.state.isUsersDownloaded
+          ? "тут нормально отрендендерить компоненты"
+          : "Данные загружаются"
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
